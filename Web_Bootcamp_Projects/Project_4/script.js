f2748651f2d5e846c0788819347b2733e3507bd1
Pierre <jonkerpierre637@gmail.com>
2024-03-25 11:48:04 +0200

    Video

--- Web_Bootcamp_Projects/Project_4/script.js
+++ Web_Bootcamp_Projects/Project_4/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here (Com out to add to GitHub)
+const API_KEY = ""; // Paste your API key here (Com out to add to GitHub)
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
@@ -19,7 +19,7 @@ const createChatLi = (message, className) => {
 }
 
 const generateResponse = (chatElement) => {
-     const API_URL = "https://api.openai.com/v1/chat/completions";
+     const API_URL = "";
     const messageElement = chatElement.querySelector("p");
 
     // Define the properties and message for the API request
